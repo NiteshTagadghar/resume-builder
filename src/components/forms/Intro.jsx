0b701bd62d942b1f17e68f3a34d178d2c8c1cafb
nitesh <niteshtagadghar3@gmail.com>
2025-11-13 12:35:03 +0530

    rendering forms dynamically

--- src/components/forms/Intro.jsx
+++ src/components/forms/Intro.jsx
@@ -1,56 +1,165 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { intro, updateIntro } from '../../features/userDataSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 function Intro() {
+
+    const [userDetail, setUserDetails] = useState(intro)
+    const storedDetails = useSelector((state)=>state.userData)
+    const dispatch = useDispatch()
+
+    function getData(e) {
+        e.preventDefault()
+        
+        // Data update in store
+        dispatch(updateIntro(userDetail))
+
+        // 
+    }
+
+    console.log(storedDetails)
     return (
         <div>
-
-
-            <form className="max-w-md mx-auto">
+            <form className="max-w-md mx-auto" onSubmit={getData}>
+                {/* First Name */}
                 <div className="relative z-0 w-full mb-5 group">
-                    <input 
-                    type="text" 
-                    name="floating_firstName" 
-                    id="floating_firstName" 
-                    className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" 
-                    placeholder=" " 
-                    required 
+                    <input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                        placeholder=" "
+                        value={userDetail.firstName}
+                        required
+                        onChange={(e) => setUserDetails({ ...userDetail, firstName: e.target.value })}
                     />
-                    <label for="floating_firstName" 
-                    className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
+                    <label
+                        htmlFor="firstName"
+                        className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
                         First Name
                     </label>
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
-                    <label for="floating_password" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password</label>
-                </div>
-                <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
-                    <label for="floating_repeat_password" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Confirm password</label>
-                </div>
-                <div className="grid md:grid-cols-2 md:gap-6">
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
-                        <label for="floating_first_name" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">First name</label>
-                    </div>
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
-                        <label for="floating_last_name" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Last name</label>
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 md:gap-6">
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
-                        <label for="floating_phone" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Phone number</label>
-                    </div>
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
-                        <label for="floating_company" className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Company (Ex. Google)</label>
-                    </div>
-                </div>
-                <button type="submit" className="text-white bg-green-500 box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Next</button>
-            </form>
 
+                {/* Last Name */}
+                <div className="relative z-0 w-full mb-5 group">
+                    <input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                        placeholder=" "
+                        value={userDetail.lastName}
+                        required
+                        onChange={(e) => setUserDetails({ ...userDetail, lastName: e.target.value })}
+
+                    />
+                    <label
+                        htmlFor="lastName"
+                        className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                        Last Name
+                    </label>
+                </div>
+
+                {/* Email */}
+                <div className="relative z-0 w-full mb-5 group">
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                        placeholder=" "
+                        value={userDetail.email}
+
+                        required
+                        onChange={(e) => setUserDetails({ ...userDetail, email: e.target.value })}
+
+                    />
+                    <label
+                        htmlFor="email"
+                        className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                        Email
+                    </label>
+                </div>
+
+                {/* LinkedIn and GitHub */}
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input
+                            type="url"
+                            name="linkedIn"
+                            id="linkedIn"
+                            className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                            placeholder=" "
+                            value={userDetail.linkedIn}
+
+                            required
+                            onChange={(e) => setUserDetails({ ...userDetail, linkedIn: e.target.value })}
+
+                        />
+                        <label
+                            htmlFor="linkedIn"
+                            className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            LinkedIn
+                        </label>
+                    </div>
+
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input
+                            type="url"
+                            name="github"
+                            id="github"
+                            className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                            placeholder=" "
+                            value={userDetail.gitHub}
+
+                            required
+                            onChange={(e) => setUserDetails({ ...userDetail, gitHub: e.target.value })}
+
+                        />
+                        <label
+                            htmlFor="github"
+                            className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Github Link
+                        </label>
+                    </div>
+                </div>
+
+                {/* Phone Number */}
+                <div className="relative z-0 w-full mb-5 group">
+                    <input
+                        type="tel"
+                        name="phone"
+                        id="phone"
+                        pattern="[0-9]{10}"
+                        className="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
+                        placeholder=" "
+                        value={userDetail.phoneNo}
+
+                        required
+                        onChange={(e) => setUserDetails({ ...userDetail, phoneNo: e.target.value })}
+
+                    />
+                    <label
+                        htmlFor="phone"
+                        className="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                        Phone number
+                    </label>
+                </div>
+
+                {/* Submit */}
+                <button
+                    type="submit"
+                    className="text-white bg-green-500 box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+                >
+                    Next
+                </button>
+            </form>
         </div>
     )
 }
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import Intro from '../components/forms/Intro'
+import { useSelector } from 'react-redux'
+import Objective from '../components/forms/Objective'
+import Skills from '../components/forms/Skills'
+import Projects from '../components/forms/Projects'
 
 function Form() {
+
+    const formRenderStatus = useSelector((state)=>state.userData.formRenders)
+
+
+    console.log(formRenderStatus,'form render status')
   return (
     <div className='h-screen flex justify-center items-center'>
-        <Intro />
+
+    {formRenderStatus.INTRO && <Intro />}
+
+    {formRenderStatus.OBJECTIVE && <Objective />}
+
+    {formRenderStatus.SKILLS && <Skills />}
+
+    {formRenderStatus.PROJECTS && <Projects />}
+
+
+    {formRenderStatus.EDUCATION && <Objective />}
+
+
+
+
     </div>
   )
 }
